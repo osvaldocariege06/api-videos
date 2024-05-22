c7c96a2d55509d37967c4b07043c7b662d80384b
osvaldocariege06 <osvaldocariege06@gmail.com>
2024-05-22 12:22:43 +0100

    First Commit

---
+++ database-postgres.js
@@ -0,0 +1,35 @@
+import { randomUUID } from "node:crypto"
+import { sql } from './db.js'
+
+export class DatabasePostgres {
+
+  async list(search) {
+    let videos;
+
+    if(search) {
+      videos = await sql`select * from videos where title ilike "${'%' + search + '%'}%"`
+    } else {
+      videos = await sql`select * from videos`
+    }
+
+    return videos
+  }
+
+  search(search){
+  
+  }
+
+  async create(video) {
+    const videoId = randomUUID()
+    const { title, description, duration  } = video
+    await sql`insert into videos (id, title, description, duration) VALUES (${videoId}, ${title},${description}, ${duration})`
+  }
+
+  update(id, video) {
+    
+  }
+
+  delete(id) {
+
+  }
+}
